efb9717ed59b8bf6c728baaf55985526099a1376
Matheus Silverio <matheusphillipo@gmail.com>
2022-07-10 01:54:11 -0300

    Upload solutions

---
+++ beecrowd/bee-1929.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define endl "\n"; 
+
+
+using namespace std;
+ 
+int main() {
+ 
+    int a,b,c,d;
+    
+    cin >> a >> b >> c >> d;
+
+    if(a < b + c && b < a + c && c < a + b){
+        cout << "S" << endl;
+    }else if(b < c + d && c < b + d && d < b + c){
+        cout << "S" << endl;
+    }else if(a < c + d && c < a + d && d < a + c){
+        cout << "S" << endl;
+    }else if(a < b + d && b < a + d && d < a + b){
+        cout << "S" << endl;
+    }else{
+        cout << "N" << endl;
+    }
+ 
+    return 0;
+}
